b0ae1b4fc9fde114a455341927a7d1d4b673350a
DavidReinon <davidreinongarcia@gmail.com>
2024-04-30 11:48:50 +0200

    Ocr screen UI

---
+++ lib/features/ocr/ocr.dart
@@ -0,0 +1,252 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Ocr extends StatelessWidget {
+  const Ocr({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(12),
+          child: Text(
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              'Optical Character Recognition'),
+        ),
+        DemoImages(),
+        SelectImageIcons(),
+        ResultTextContainer(),
+      ],
+    );
+  }
+}
+
+class ResultTextContainer extends StatefulWidget {
+  const ResultTextContainer({
+    super.key,
+  });
+
+  @override
+  State<ResultTextContainer> createState() => _ResultTextContainerState();
+}
+
+class _ResultTextContainerState extends State<ResultTextContainer> {
+  String _resultText = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: Container(
+            width: 320,
+            height: 350,
+            margin: const EdgeInsets.all(12),
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: const Color.fromARGB(255, 5, 0, 0),
+                width: 2,
+              ),
+            ),
+            child: Text(
+              _resultText,
+              style: const TextStyle(fontSize: 16),
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Add your button 1 logic here
+              },
+              child: const Text('CLEAR'),
+            ),
+            const SizedBox(width: 15),
+            ElevatedButton(
+              onPressed: () {
+                // Add your button 2 logic here
+              },
+              child: const Text('COPY ALL'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class SelectImageIcons extends StatelessWidget {
+  const SelectImageIcons({
+    super.key,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(8.0, 8, 8.0, 3.0),
+          child: Text(
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 10, fontStyle: FontStyle.italic),
+              'You can use the camera or gallery to select an image. '),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ButtonWithText(
+              color: Color.fromARGB(255, 33, 219, 39),
+              icon: Icons.camera_alt,
+              label: 'Camera',
+            ),
+            SizedBox(width: 15),
+            ButtonWithText(
+              color: Color.fromARGB(255, 36, 125, 18),
+              icon: Icons.image,
+              label: 'Gallery',
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  Future<XFile?> _openCamera() async {
+    final picker = ImagePicker();
+    XFile? pickedFile = await picker.pickImage(source: ImageSource.camera);
+    return pickedFile;
+  }
+
+  Future<XFile?> _openGallery() async {
+    final picker = ImagePicker();
+    XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    return pickedFile;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _openGallery();
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DemoImages extends StatelessWidget {
+  const DemoImages({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+          child: Text(
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 10, fontStyle: FontStyle.italic),
+              'Here are some sample images you can try (Tap): '),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: SizedBox(
+                height: 70, // Adjust this to the height you need
+                width: 320, // Adjust this to the width you need
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: const [
+                    ImageWithBorder(
+                        imagePath: 'assets/demo_national_identity_card.jpg',
+                        width: 100,
+                        fit: BoxFit.fitHeight),
+                    ImageWithBorder(
+                        imagePath: 'assets/demo_postit.jpg',
+                        width: 100,
+                        fit: BoxFit.fitWidth),
+                    ImageWithBorder(
+                        imagePath: 'assets/demo_recipe.jpg',
+                        width: 100,
+                        fit: BoxFit.fitHeight),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class ImageWithBorder extends StatelessWidget {
+  const ImageWithBorder({
+    super.key,
+    required this.imagePath,
+    required this.width,
+    required this.fit,
+  });
+
+  final String imagePath;
+  final double width;
+  final BoxFit fit;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Add your image tap logic here
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+            width: 1,
+          ),
+        ),
+        child: Image.asset(
+          imagePath,
+          width: width,
+          fit: fit,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'features/ocr/ocr.dart';
+import 'features/settings/settings.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AzureAI Flutter Demo',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 137, 236, 71),
+        ),
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 137, 236, 71)),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _tabs = [
+    const Ocr(),
+    const Settings(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('AzureAI Flutter Demo')),
+      ),
+      body: _tabs[_currentIndex],
+      bottomNavigationBar: Material(
+        color: Theme.of(context).splashColor,
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'OCR',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
