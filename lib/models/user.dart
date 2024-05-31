9e0fdb8368d606b00f1e8bbc1a00f4d95c7c7158
DavidReinon <davidreinongarcia@gmail.com>
2024-05-31 17:04:13 +0200

    reafactor: Add getters to user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,9 +1,19 @@
 class User {
+  User({this.id, this.name, this.password});
+  
+  User.fromMap(Map<String, dynamic> map)
+    : id = map['id'],
+      name = map['name'],
+      password = map['password'];
+
   final int? id;
   final String? name;
   final String? password;
 
-  User({this.id, this.name, this.password});
+  int? get getId => id;
+  String? get getName => name;
+  String? get getPassword => password;
+
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> json = {};
