7168186b1a4c09e9f03e30aaf3c2ff02d90cbba8
DavidReinon <davidreinongarcia@gmail.com>
2024-05-31 17:24:59 +0200

    fix: Add scroll view to control overflowed pixels using keyboard

--- lib/features/authentication/widgets/log_in_screen.dart
+++ lib/features/authentication/widgets/log_in_screen.dart
@@ -62,57 +62,59 @@ class LogInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Text(
-              'Log In',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const Text(
-              'Log in to your account to acces to OCR!',
-              style: TextStyle(fontSize: 15),
-            ),
-            const SizedBox(height: 30.0),
-            Image.asset(
-              'assets/picture_avatar_green.png',
-              width: 200,
-              height: 200,
-            ),
-            const SizedBox(height: 15.0),
-            Column(
-              children: [
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Name',
-                    border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Log In',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                'Log in to your account to acces to OCR!',
+                style: TextStyle(fontSize: 15),
+              ),
+              const SizedBox(height: 30.0),
+              Image.asset(
+                'assets/picture_avatar_green.png',
+                width: 200,
+                height: 200,
+              ),
+              const SizedBox(height: 15.0),
+              Column(
+                children: [
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: _nameController,
                   ),
-                  controller: _nameController,
-                ),
-                const SizedBox(height: 30.0),
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
+                  const SizedBox(height: 30.0),
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: _passwordController,
+                    obscureText: true,
                   ),
-                  controller: _passwordController,
-                  obscureText: true,
-                ),
-                const SizedBox(height: 30.0),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(const Size(350, 30)),
+                  const SizedBox(height: 30.0),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(const Size(350, 30)),
+                    ),
+                    onPressed: () {
+                      onPressed(context);
+                    },
+                    child: const Text('Log In', style: TextStyle(fontSize: 16)),
                   ),
-                  onPressed: () {
-                    onPressed(context);
-                  },
-                  child: const Text('Log In', style: TextStyle(fontSize: 16)),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/authentication/widgets/sign_up_screen.dart
+++ lib/features/authentication/widgets/sign_up_screen.dart
@@ -52,66 +52,68 @@ class SignUpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Text(
-              'Sign Up',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const Text(
-              'Crate new account and get started with OCR!',
-              style: TextStyle(fontSize: 15),
-            ),
-            const SizedBox(height: 30.0),
-            Image.asset(
-              'assets/picture_avatar_green.png',
-              width: 200,
-              height: 200,
-            ),
-            const SizedBox(height: 15.0),
-            Column(
-              children: [
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Name',
-                    border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Sign Up',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                'Crate new account and get started with OCR!',
+                style: TextStyle(fontSize: 15),
+              ),
+              const SizedBox(height: 30.0),
+              Image.asset(
+                'assets/picture_avatar_green.png',
+                width: 200,
+                height: 200,
+              ),
+              const SizedBox(height: 15.0),
+              Column(
+                children: [
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: _nameController,
                   ),
-                  controller: _nameController,
-                ),
-                const SizedBox(height: 30.0),
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
+                  const SizedBox(height: 30.0),
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: _passwordController,
+                    obscureText: true,
                   ),
-                  controller: _passwordController,
-                  obscureText: true,
-                ),
-                const SizedBox(height: 30.0),
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Confirm Password',
-                    border: OutlineInputBorder(),
+                  const SizedBox(height: 30.0),
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'Confirm Password',
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: _confirmPasswordController,
+                    obscureText: true,
                   ),
-                  controller: _confirmPasswordController,
-                  obscureText: true,
-                ),
-                const SizedBox(height: 30.0),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(const Size(350, 30)),
+                  const SizedBox(height: 30.0),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(const Size(350, 30)),
+                    ),
+                    onPressed: () {
+                      onPressed(context);
+                    },
+                    child: const Text('Sign Up', style: TextStyle(fontSize: 16)),
                   ),
-                  onPressed: () {
-                    onPressed(context);
-                  },
-                  child: const Text('Sign Up', style: TextStyle(fontSize: 16)),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
